e882733605a7dcb20ffa9eb40a29b64bbdbae069
talalfi <alfital2@gmail.com>
2021-01-11 17:46:27 +0200

    my runner can now send requests to the server and recieve answers

--- client/my-runner/run.js
+++ client/my-runner/run.js
@@ -1,22 +1,24 @@
 const fs = require('fs');
+const axios = require('axios');
 const { pass, fail } = require('create-jest-runner');
+const URL = 'http://localhost:3000';
 
-module.exports = ({ testPath }) => {
+module.exports = async ({ testPath }) => {
   const start = Date.now();
   const contents = fs.readFileSync(testPath, 'utf8');
   const end = Date.now();
 
   const res = await axios.post(URL);
+  const msg = 'communication works! '+res.data;
 
-  console.log(res.data);
+  return fail({ 
+    start, 
+    end, 
+    test: {
+        path: testPath ,
+        errorMessage:msg
+      } 
+    });
 
-  // if (contents.includes('⚔️🏃')) {
-  //   return pass({ start, end, test: { path: testPath } });
-  // }
-  // const errorMessage = 'Company policies require ⚔️ 🏃 in every file';
-  // return fail({
-  //   start,
-  //   end,
-  //   test: { path: testPath, errorMessage, title: 'Check for ⚔️ 🏃' },
-  // });
+
 };
