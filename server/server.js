951a3859a07c22463fdbd474d9cb01d0d5b45f47
talalfi <alfital2@gmail.com>
2021-01-12 15:17:43 +0200

    test are now being executed on the server, and the result can be now sent to the client

--- server/server.js
+++ server/server.js
@@ -1,19 +1,28 @@
 'use strict';
 const express = require('express');
 const fs = require('fs');
+const jest = require('jest');
 
 const PORT = 3000;
 const HOST = '0.0.0.0';
+const TEST_RESULT_FILE = 'res.json';
 
 const app = express();
 app.use(express.json());
 
-app.post('/', (req, res) => {   
+app.post('/', async (req, res) => {   
     fs.writeFileSync(req.body.fileName, req.body.testText, {
         flag: 'w+',
       });
-    res.send('ok');
-});
+      await jest.run(
+        // eslint-disable-next-line max-len
+        `npm test --json --outputFile=${TEST_RESULT_FILE} --detectOpenHandles`,
+    );
+
+    const testResult = fs.readFileSync(TEST_RESULT_FILE, 'utf8');
+    res.send(testResult);
+  }
+);
 
 app.listen(PORT, HOST);
 console.log(`Running on http://${HOST}:${PORT}`);
