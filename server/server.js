27060c479946ff898457389bb8659e82e5d2e216
talalfi <alfital2@gmail.com>
2021-01-13 18:28:02 +0200

    now each on the server has a unique id

--- server/server.js
+++ server/server.js
@@ -10,19 +10,28 @@ const TEST_RESULT_FILE = 'res.json';
 const app = express();
 app.use(express.json());
 
+const uuid = function uuidv4() {
+  return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
+    let r = Math.random() * 16 | 0, v = c == 'x' ? r : (r & 0x3 | 0x8);
+    return v.toString(16);
+  });
+}
+
+
 app.post('/', async (req, res) => {   
-    fs.writeFileSync(req.body.fileName, req.body.testText, {
+  const uniqueFileName = uuid()+'.spec.js';
+    fs.writeFileSync(uniqueFileName, req.body.testText, {
         flag: 'w+',
       });
       await jest.run(
         // eslint-disable-next-line max-len
-        ` --json --outputFile=${TEST_RESULT_FILE} --detectOpenHandles`,
+        ` --json --outputFile=${TEST_RESULT_FILE}  --findRelatedTests ${uniqueFileName} --detectOpenHandles `,
     );
 
     const testResult = fs.readFileSync(TEST_RESULT_FILE, 'utf8');
 
     fs.unlinkSync(TEST_RESULT_FILE);
-    fs.unlinkSync('test.spec.js');
+    fs.unlinkSync(uniqueFileName);
     res.send(testResult);
   }
 );
