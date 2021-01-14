3e41a5e12b77ba4b9e0e7d8a1822edd8e7f10123
talalfi <alfital2@gmail.com>
2021-01-14 14:27:20 +0200

    refactoring - changing from promise based structure to async await structure

--- client/my-runner/run.js
+++ client/my-runner/run.js
@@ -6,39 +6,17 @@ const URL = 'http://localhost:3000';
 module.exports = async ({ testPath }) => {
   const contents = fs.readFileSync(testPath, 'utf8');
   const start = Date.now();
-  return await axios
-  .post(URL, {
-    fileName: 'test.spec.js',
-    testText: contents,
-  })
-  .then((res) => {
-    return res.data;
-  })
-  .then((result) => {
-    const end = Date.now();
 
-    if (result.success) {
-      return pass({
-        start,
-        end,
-        test: {
-          path: testPath,
-        },
-      });
-    }
-    const errorMessage = result.testResults[0].message;
-    return fail({
-      start,
-      end,
-      test: {
-        path: testPath,
-        errorMessage,
-        title: 'failed',
-      },
-    });
-  })
-  .catch((error) => {
-    return error;
-  });
+  const respone = await axios.post(URL, {testText: contents,})
+  const result = respone.data;
+  const end = Date.now();
+
+  if (result.success) {
+        return pass({start,end,test: {path: testPath}});
+  }
   
+  // test failed
+  const errorMessage = result.testResults[0].message;
+  return fail({start,end,test: {path: testPath,errorMessage,title: 'failed'}});
+
 };
